cf392706dca025c49a7985191c7ba9f723fd8561
powegame <powegamestudio@gmail.com>
2024-02-29 19:31:12 +0700

    animal

--- AnimalProject/Program.cs
+++ AnimalProject/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StopwatchProject
+namespace AnimalProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Cat cat = new Cat("20kg", "1.5", "kitty");
+            cat.PrintInfo();
         }
     }
 }
--- AnimalProject/Properties/AssemblyInfo.cs
+++ AnimalProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StopwatchProject")]
+[assembly: AssemblyTitle("AnimalProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StopwatchProject")]
+[assembly: AssemblyProduct("AnimalProject")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b778ec28-d9e3-45ce-b66d-d66f5ad853e8")]
+[assembly: Guid("84e62033-68a8-4004-8b14-348800baaf7a")]
 
 // Version information for an assembly consists of the following four values:
 //
