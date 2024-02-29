22473277114ac0a05f6b271c0ebf5fed39c859f8
powegame <powegamestudio@gmail.com>
2024-02-29 18:26:15 +0700

    Lớp hình chữ nhật

---
+++ Rectangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input width:");
+            double width = Double.Parse(Console.ReadLine());
+            Console.Write("Input height:");
+            double height = Double.Parse(Console.ReadLine());
+            Rec rec = new Rec(width, height);
+
+            Console.WriteLine("Your Rectangle " + rec.Display());
+            Console.WriteLine("Perimeter of the Rectangle: " + rec.GetPerimeter());
+            Console.WriteLine("Area of the Rectangle: " + rec.GetArea());
+            Console.ReadLine();
+        }
+    }
+}
