5e5618f27cbdb8b835469327c7e13b1d76ef326e
powegame <powegamestudio@gmail.com>
2024-03-06 20:59:11 +0700

    [Thực hành] lớp Animal và interface Edible

---
+++ AnimalInterface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[2];
+            animals[0] = new Tiger();
+            animals[1] = new Chicken();
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal.MakeSound());
+            }
+
+            Fruit[] fruits = new Fruit[2];
+            fruits[0] = new Orange();
+            fruits[1] = new Apple();
+            foreach (Fruit fruit in fruits)
+            {
+                Console.WriteLine(fruit.HowToEat());
+            }
+        }
+    }
+}
