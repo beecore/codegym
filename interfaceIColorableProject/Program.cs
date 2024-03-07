7d2acb178a02dc354b0779d9a8c43736ac1c96ec
powegame <powegamestudio@gmail.com>
2024-03-07 14:30:02 +0700

    [Bài tập] Triển khai interface ‘IColorable’ cho các lớp hình học

---
+++ interfaceIColorableProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaceIColorableProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Shape[] shapes = new Shape[3];
+            shapes[0] = new Square(5);
+            shapes[1] = new Circle(3);
+            shapes[2] = new Rectangle(2,4);
+
+           
+            Square square = shapes[0] as Square;
+            Circle circle = shapes[1] as Circle;
+            Rectangle rectangle = shapes[2] as Rectangle;
+
+            Console.WriteLine("---- Area");
+            Console.WriteLine(circle.getArea());
+            Console.WriteLine(rectangle.getArea());
+            Console.WriteLine(square.getArea());
+
+            square.HowToColor();
+           
+
+
+
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ interfaceIColorableProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("interfaceIColorableProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("interfaceIColorableProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59181f6f-a495-4849-95cc-8e9be9b88688")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
