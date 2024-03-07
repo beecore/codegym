793f8d083942f27ac8d61f3c5292ca2ecec3df9c
powegame <powegamestudio@gmail.com>
2024-03-07 19:17:12 +0700

    [Thực hành] Tính tổng các số trong file text

---
+++ FileFileInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFilePath = @"C:\MyData\a\a.txt"; //File nguồn
+            string destinationFilePath = @"C:\MyData\b\b.txt"; //File đích
+            bool overWrite = true; //Có ghi đè không
+           // File.Copy(sourceFilePath, destinationFilePath, overWrite);
+            FileInfo fInfo = new FileInfo(sourceFilePath);
+            fInfo.CopyTo(destinationFilePath, overWrite);
+            string directoryPath = fInfo.DirectoryName;
+            //xoa
+            // sourceFilePath = @"C:\MyData\b\b.txt";
+            //File.Delete(sourceFilePath);
+
+            //check
+            //bool doesFileExist = File.Exists(sourceFilePath);
+            //DateTime fileCreatedOn = File.GetCreationTime(sourceFilePath);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ W8_0_sumNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W8_0_sumNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFilePath = @"C:\MyData\a\numbers.txt"; //File nguồn
+            Console.WriteLine("File path: " + sourceFilePath);
+            ReadTextFileExample example = new ReadTextFileExample();
+            example.ReadTextFile(sourceFilePath);
+            Console.ReadLine();
+        }
+    }
+}
