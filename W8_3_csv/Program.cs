a3b4e6d99ade80a1492ca05000abbaa87ba1d743
powegame <powegamestudio@gmail.com>
2024-03-09 13:12:12 +0700

    [Bài tập] Đọc file .csv GU1-W08  File & Serialization  [Bài tập] Đọc file .csv

---
+++ W8_3_csv/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace W8_3_csv
+{
+    internal class Country
+    {
+        public string IPX { get; set; }
+        public string IPY { get; set; }
+        public string CorX { get; set; }
+        public string CorY { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+
+    internal class ReadWritecsv
+    {
+        public static void ReadCSV()
+        {
+            StreamReader reader = new StreamReader("C:\\MyData\\data.csv");
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                string[] values = line.Split(',');
+                foreach (var value in values)
+                {
+                    Console.Write(value + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ReadWritecsv.ReadCSV();
+            Console.ReadLine();
+        }
+    }
+}
